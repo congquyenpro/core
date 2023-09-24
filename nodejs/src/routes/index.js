ec8f4c820e2c6bb7ac67a7d08b24a09f06d504c0
congquyenpro <quyen.nc207699@sis.hust.edu.vn>
2023-09-24 09:11:52 +0700

    update

--- nodejs/src/routes/index.js
+++ nodejs/src/routes/index.js
@@ -1,5 +1,5 @@
 const newsRouter = require('./news');
-
+const siteRouter = require('./site');
 
 function route(app){
 
@@ -7,17 +7,7 @@ function route(app){
 
 
 
-
-/*     app.get('/home', (req, res) => {
-    res.render('home');
-    });
-    app.get('/search', (req, res) => {
-        res.render('search');
-    });
-    app.post('/search', (req, res) => {
-        console.log(req.body);
-        res.send("");
-    }); */
+    app.use('/', siteRouter);
     
 }
 
